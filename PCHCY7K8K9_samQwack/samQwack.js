ce781da5529fa4012ed3ac4ac29097886fea8e9b
MukkaSamhith <162540300+MukkaSamhith@users.noreply.github.com>
2024-04-04 17:31:40 +0430

    Add files via upload

---
+++ PCHCY7K8K9_samQwack/samQwack.js
@@ -0,0 +1,7 @@
+function myWordDay() {
+    location.replace("https://www.qwackedu.com/vocabulary/word")
+}
+
+function playFunGames() {
+    location.replace("https://www.qwackedu.com/vocabulary/games")
+}
